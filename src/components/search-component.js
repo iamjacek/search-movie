fc64d6b71444a80669cbbb91674e27ad374763d1
iamjacek <jake47s@gmail.com>
2021-03-05 00:09:01 +0000

    new componend - card, style

--- src/components/search-component.js
+++ src/components/search-component.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
+import MovieCard from "./movieCard-component"
 
 const SearchComponent = () => {
   const [query, setQuery] = useState("")
@@ -16,9 +17,9 @@ const SearchComponent = () => {
       const res = await fetch(url)
       const data = await res.json()
       // setMovies(JSON.stringify(data.results))
-      let titles = []
-      data.results.map((x) => titles.push(x))
-      setMovies(titles)
+      let collection = []
+      data.results.map((x) => collection.push(x))
+      setMovies(collection)
     } catch (err) {
       console.error(err)
     }
@@ -43,27 +44,7 @@ const SearchComponent = () => {
       </form>
       <div className="movie-list">
         {movies.map((movie) => (
-          <div className="movie-card" key={`movie_key_${movie.id}`}>
-            {movie.poster_path === null ? (
-              <p>poster not available :(</p>
-            ) : (
-              <img
-                className="movie-card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + " poster"}
-              />
-            )}
-            <div className="movie-card--content">
-              <h2 className="movie-card--title">{movie.title}</h2>
-              <p>
-                <small>RELEASE DATE: {movie.release_date}</small>
-              </p>
-              <p>
-                <small>RATING: {movie.vote_average}</small>
-              </p>
-              <p className="card--desc">{movie.overview}</p>
-            </div>
-          </div>
+          <MovieCard {...movie} key={movie.id} />
         ))}
       </div>
     </>
